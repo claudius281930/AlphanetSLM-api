7b2da1f2d245854c20422192942618bcfb5a9a78
claudiusCCS <claudiuslapidar@gmail.com>
2023-05-12 15:52:42 -0300

    Só uns ajustes

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const mainController = require("../controllers/mainController");
 
 //C;
@@ -8,15 +7,14 @@ router.post("/create/box", mainController.createBox);
 router.post("/create/fusion", mainController.createFusion);
 router.post("/create/color", mainController.createColor);
 router.post("/create/link", mainController.createLink);
-
+/* --------------------------------------------------------------------- */
 //R;
-//Rotas de consulta geral;
 router.get("/",mainController.findBox);
 router.get("/box", mainController.findBox);
 router.get("/fusion", mainController.findFusion);
 router.get("/color", mainController.findColor);
 router.get("/link", mainController.findLink);
-
+/* --------------------------------------------------------------------- */
 //Rotas de consultas especificas e detalhadas;
 router.get("/:id", mainController.findByIdBox);
 router.get("/fusion/:id", mainController.findByIdFusion);
@@ -25,11 +23,11 @@ router.get("/link/:id", mainController.findByIdLink);
 router.get("/name/:name_description",mainController.findByName); 
 router.get('/locale/:locale', mainController.findByLocale);
 router.get("/detail/:name_description", mainController.findDetail);
-
+/* --------------------------------------------------------------------- */
 //U;
 router.put("/:id", mainController.fullUpdate);
-//router.patch("/:id", mainController.partialUpdate);
-
+//router.patch("/:id", mainController.partialUpdate); //Não há necessidade de implentação;
+/* --------------------------------------------------------------------- */
 //D;
 router.delete("/:id", mainController.destroy);
 
